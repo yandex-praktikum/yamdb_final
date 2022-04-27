04ef92783a1935ff66b25ae0f892f72f403d5441
Andrey Shadrin <shdrn@yandex.ru>
2022-04-27 15:27:58 +0300

    test

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -13,7 +13,7 @@ SECRET_KEY = os.getenv(
     'SECRET_KEY', default='my_secret_key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
